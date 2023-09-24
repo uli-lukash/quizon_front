b161b0574f0e2435e332eaab16befbeb3b7ed493
Lukash Sergey Davidovich <slukash@ozon.ru>
2023-09-24 21:06:02 +0300

    [fix] fix loop x3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         let i = 0;
         while (i < arr.length) {
             if (arr[i].value == "") {
-                arr[].style.borderColor="red";
+                arr[i].style.borderColor="red";
                 i = i + 1;
             }
         }
